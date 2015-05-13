6576d3faff5d6398a120fd7756c547a62b0ceb5a
Black-Humor <bobsnake64@gmail.com>
2015-05-12 20:19:15 -0500

    Background scrolls infinitely. (reverted from commit effc8bb50d650c51894e91265e412100386d8388)

--- Assets/BackgroundCycler.cs
+++ Assets/BackgroundCycler.cs
@@ -4,15 +4,21 @@ using System.Collections;
 public class BackgroundCycler : MonoBehaviour {
 
 	const float WIDTH = 22;
-	const float NUM_PIECES = 4;
 
-	void OnTriggerEnter2D(Collider2D c){
-		Debug.Log ("Collision!");
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision c){
 		GameObject otherObject = c.gameObject;
-		Debug.Log (otherObject.ToString());
 		if (otherObject.tag == "Background"){
-			otherObject.transform.position += Vector3.right * WIDTH * NUM_PIECES;
-			Debug.Log("Background Cycled!");
+			otherObject.transform.position.Set(otherObject.transform.position.x + WIDTH, otherObject.transform.position.y, otherObject.transform.position.z);
 		}
 	}
 }
