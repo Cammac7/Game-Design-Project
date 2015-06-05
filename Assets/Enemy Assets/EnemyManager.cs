bd87ca210bfc8d10a1e08fdee4c65e8dc0056b0c
Black-Humor <bobsnake64@gmail.com>
2015-06-05 00:06:30 -0500

    Revert "Spawners no longer spwan infinitely.", because it makes me crash.
    This reverts commit 40625684592b050fb27afc069802316b98ed7888.

--- Assets/Enemy Assets/EnemyManager.cs
+++ Assets/Enemy Assets/EnemyManager.cs
@@ -6,21 +6,17 @@ public class EnemyManager : MonoBehaviour {
 
 	public GameObject Enemy;
 	public float spawnTime = 3f;
-	public int numSpawns = 5;
 	public Transform[] spawnPoints;
 
-	private int alreadySpawned = 0;
 
 	// Use this for initialization
 	void Start () {
-		while (alreadySpawned < numSpawns){
-			InvokeRepeating ("Spawn", spawnTime, spawnTime);
-		}
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
 	}
 	
 	// Update is called once per frame
 	void Spawn () {
 		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 		Instantiate (Enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
-	}
+}
 }
