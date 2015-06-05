3d6f5fff277c91ba2ca32cada804380e2f36aaf3
Black-Humor <bobsnake64@gmail.com>
2015-06-05 01:31:06 -0500

    General level balance stuff.

--- Assets/Enemy Assets/EnemyManager.cs
+++ Assets/Enemy Assets/EnemyManager.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class EnemyManager : MonoBehaviour {
 
 	public GameObject Enemy;
 	public float spawnTime = 3f;
 	public int numSpawns = 5;
+	public float spawnDistance = 20;
 
 	private int spawnedAlready;
 	private float timeOfLastSpawn;
+	private GameObject player;
+
+	void Awake() {
+		player = GameObject.Find("Player");
+		//Debug.Log((player == null));
+	}
 
 	void Update(){
-		if (spawnedAlready < numSpawns && Time.time > (timeOfLastSpawn + spawnTime)){
+		//Debug.Log((player == null));
+		float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+		if (spawnedAlready < numSpawns 
+		    && Time.time > (timeOfLastSpawn + spawnTime)
+		    && distanceToPlayer < spawnDistance
+		    ) {
+
 			timeOfLastSpawn = Time.time;
 			spawnedAlready++;
 			Spawn();
